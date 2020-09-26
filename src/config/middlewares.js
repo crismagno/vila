192f4bd407608a6ea747c536085d1f1070d03515
cristhofer <cristhofer.mendonca@gmail.com>
2020-09-26 12:11:56 -0300

    Refactoring route users

--- src/config/middlewares.js
+++ src/config/middlewares.js
@@ -1,10 +1,10 @@
-const bodyPArser = require("body-parser")
+const bodyParser = require("body-parser")
 const cors = require("cors")
 const logger = require('morgan')
 
 module.exports = app => {
     app.use(logger('dev'));
-    app.use(bodyPArser.json())
-    app.use(bodyPArser.urlencoded({ extended: true }))
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({ extended: true }))
     app.use(cors())
 }
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,7 +1,5 @@
-const router = require('express').Router()
 const path = require('path')
 const multer = require('multer')
-
 const Storage = multer.diskStorage({
     destination(req, file, callback) {
         callback(null, path.join(__dirname,'../upload_images'))
@@ -11,18 +9,14 @@ const Storage = multer.diskStorage({
         callback(null, `${file.fieldname}_${file.originalname}_${Date.now()}.${type}`)
     },
 })
-    
 const upload = multer({ storage: Storage })
 
 module.exports = app => {
-
-    router.post('/', app.users.create)
-    router.put('/avatar/:id', upload.single('avatar'), app.users.updateAvatar)
-    router.put('/:id', app.users.updateUser)
-    router.delete('/:id', app.users.removeUser)
-    router.get('/', app.users.findAll)
-    router.get('/signIn', app.users.findUserLogin)
-    router.get('/:id', app.users.findUser)
-    
-    app.use('/users', router)
+    app.post('/users/', app.users.create)
+    app.put('/users/avatar/:id', upload.single('avatar'), app.users.updateAvatar)
+    app.put('/users/:id', app.users.updateUser)
+    app.delete('/users/:id', app.users.removeUser)
+    app.get('/users/', app.users.findAll)
+    app.get('/users/signIn', app.users.findUserLogin)
+    app.get('/users/:id', app.users.findUser)
 }
